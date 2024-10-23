6e05e7244ede66719bf179b834cc96a96d31607a
Dapman601 <pohema1987@gmail.com>
2024-10-23 13:10:20 +0100

    Update 1_main.py

--- pages/1_main.py
+++ pages/1_main.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # Load the data from the specified path
 @st.cache_data
 def load_data():
-    df = pd.read_csv('https://github.com/Dapman601/EduPrime-Analytics/blob/main/school_student_data.csv')  # Your dataset path
+    df = pd.read_csv('https://raw.githubusercontent.com/Dapman601/EduPrime-Analytics/refs/heads/main/school_student_data.csv')  # Your dataset path
     return df
 
 df = load_data()
