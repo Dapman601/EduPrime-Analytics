13bbcfcf6722fc6b8c71388c845ab82c9e6c632e
Dapman601 <pohema1987@gmail.com>
2024-10-23 13:22:45 +0100

    Update 8_main8.py

--- pages/8_main8.py
+++ pages/8_main8.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 # Load data
-df = pd.read_csv(r'C:\Users\DELL\Documents\02_consults\code2\school_student_data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/Dapman601/EduPrime-Analytics/refs/heads/main/school_student_data.csv')
 
 # Sidebar filters with cascading behavior
 def apply_filters(df):
