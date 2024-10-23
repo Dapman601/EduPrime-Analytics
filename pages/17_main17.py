eafa932626371aa68ebccf642b6a60f189d4e7e4
Dapman601 <pohema1987@gmail.com>
2024-10-23 13:39:22 +0100

    Update 17_main17.py

--- pages/17_main17.py
+++ pages/17_main17.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px
 
 # Load the dataset
-df = pd.read_csv(r'C:\Users\DELL\Documents\02_consults\code2\school_student_data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/Dapman601/EduPrime-Analytics/refs/heads/main/school_student_data.csv')
 
 
 
